b4e5a3e75606a5ae1dc6e765ecbd7ce4f2cb87df
Liva Cengiz <liva@Livas-MacBook-Air.local>
2015-06-29 11:50:05 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+### [yeesno.com](http://yeesno.com)
+#Yes or No site generator
+
+##Usage
+
+##Project requirements
+- Python 3.4
+- Flask
+- Pymongo | MongoClient
+- slugify
+```
+Mongolab configuration
+```
+##Mongolab requirements
+- Mongolab user
+- Mongolab Database
+
+app.py>
+
+```
+app = Flask(__name__)
+mongoclient = MongoClient('mongodb://<dbuser>:<dbpassword>@ds041032.mongolab.com:41032/<dbtitle>'XXXX')
+
+dbtitle : Database title
+dbuser  : Mongolab username
+dbpassword : Database password
+
+```
+###Support
+- [Halil Kaya](http://github.com/halilkaya)
---
+++ app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import render_template, request, redirect, abort
+from pymongo import MongoClient
+import re
+from slugify import slugify
+import unicodedata
+
+app = Flask(__name__)
+mongoclient = MongoClient('mongodb://<dbuser>:<dbpassword>@ds041032.mongolab.com:41032/<dbtitle>')
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/<question>', methods = ['POST', 'GET'])
+def voila(question):
+    if request.method == 'GET':
+        yorn = mongoclient.yorn.yorns.find_one({"slug": question})
+        if yorn is None:
+            abort(404)
+        data = {
+            "question": yorn['question'],
+            "answer": yorn['answer']
+        }
+        return render_template("voila.html", data=data)
+    if request.method == 'POST':
+        question = request.form.get('ques')
+        answer = request.form.get('answer')
+        slug = slugify(request.form.get('ques')).replace('-', '') + '.com'
+        data = {
+            "question": question,
+            "answer": answer,
+            "slug": slug
+        }
+        mongoclient.yorn.yorns.insert(data)
+        return render_template ("voila.html", data=data)
+    else:
+        return render_template("voila.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
