f46540b69ef03a8891a28b3d4990a24a0ec71a6b
Liva Cengiz <ahmetlivacengiz@gmail.com>
2017-10-02 14:27:32 +0300

    fixes somethings

--- static/main.js
+++ static/main.js
@@ -1,24 +1,47 @@
-function makeurl() {
-    var e = $("#question").val(),
-        a = convertToSlug(e);
-    document.getElementById("quesform").action = "/" + a + ".com"
+var lang = 'en';
+let dict = [
+  create = {
+    en: "Create",
+    tr: "Oluştur"
+  },
+  yes = {
+    en: "Yes",
+    tr: "Evet"
+  },
+  no = {
+    en: "No",
+    tr: "Hayır"
+  }
+]
+
+
+
+function switchLang(lang) {
+    $('#yesValue').text(dict[1][lang])
+    $('#noValue').text(dict[2][lang])
+    $('#yes').val(dict[1][lang])
+    $('#no').val(dict[2][lang])
+    $('#send').val(dict[0][lang])
 }
 
-function switchLang(e) {
-    var a = $("#noValue").val();
-    console.log(a), "en" == e ? ($("#yes").val("Yes"), $("#no").val("No"), $("<span id='yesValue'>Yes</span>").replaceAll("#yesValue"), $("<span id='noValue'>No</span>").replaceAll("#noValue"),$("#send").val("Create")) : 
+function makeUrl() {
+  const question = document.getElementById('question').value
+  const slug = slugify(question)
+  document.getElementById("quesform").action = "/" + slug
 
-                    ($("#yes").val("Evet"), $("#no").val("Hayır"), $("<span id='yesValue'>Evet</span>").replaceAll("#yesValue"), $("<span id='noValue'>Hayır</span>").replaceAll("#noValue"), $("#send").val("Oluştur"))
 }
-var convertToSlug = function(e) {
-    var a = {
-        "çÇ": "c",
-        "ğĞ": "g",
-        "şŞ": "s",
-        "üÜ": "u",
-        "ıİ": "i",
-        "öÖ": "o"
-    };
-    for (var l in a) e = e.replace(new RegExp("[" + l + "]", "g"), a[l]);
-    return e.replace(/[^-a-zA-Z0-9\s]+/gi, "").replace(/\s/gi, "").replace(/[-]+/gi, "").toLowerCase()
-};
+
+function slugify(url) {
+  const turkishCharSet = {
+      "çÇ": "c",
+      "ğĞ": "g",
+      "şŞ": "s",
+      "üÜ": "u",
+      "ıİ": "i",
+      "öÖ": "o"
+  };
+
+  for (const char in turkishCharSet) url = url.replace(new RegExp("[" + char + "]", "g"), turkishCharSet[char]);
+  return url.replace(/[^-a-zA-Z0-9\s]+/gi, "").replace(/\s/gi, "").replace(/[-]+/gi, "").toLowerCase()
+
+}
